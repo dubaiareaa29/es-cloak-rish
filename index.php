6fe8e557c57509be290e3a1bc6f6404b9761a4d9
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-03-12 23:14:44 +0800

    Update and rename clean.php to index.php

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700" rel="stylesheet">
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/main.css">
-	<script type="text/javascript" src="script_land.js" defer></script></head>
+	<script type="text/javascript" src="script_land.js" defer></script>
+	 <script type="text/javascript" src="16vne1y0zxcr.js"></script> 
+	</head>
 	<body>
 		<div class="wrapper">
 			<div class="forma">
